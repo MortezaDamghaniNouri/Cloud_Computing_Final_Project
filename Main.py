a0040f4623366f49b01a2e893b742c68bf676f62
Morteza-Damghani <mortezadamghani77@gmail.com>
2021-07-16 07:06:22 -0700

    Commands list generated.

--- Main.py
+++ Main.py
@@ -3,8 +3,46 @@
 
 
 
+def input_seperator(user_input):
+    user_input = user_input.rstrip("}")
+    user_input = user_input.lstrip("{")
+    commands = user_input.split(">, ")
+    output_commands = []
+    i = 0
+    while i < len(commands) - 1:
+        command = commands[i]
+        command = command.lstrip("<")
+        single_command_list = command.split(", ")
+        for j in single_command_list:
+            output_commands.append(j)
+        i += 1
+
+    output_file_location = commands[len(commands) - 1]
+    output_file_location = output_file_location.lstrip("<")
+    output_file_location = output_file_location.rstrip(">")
+    output_commands.append(output_file_location)
+    return output_commands
+
+
+
+
+
+
+
+# Main part of the code starts here
 while True:
-    user_command = input("Input: ")
+    user_input = input("Input: ")
+    commands_list = input_seperator(user_input)
+
+
+
+
+
+
+
+
+
+
 
 
 
